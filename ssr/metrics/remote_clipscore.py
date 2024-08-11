46724275d94c77844cce2dff5dc2171d28925f16
niklasbloecker <n.bloeckerq@t-online.de>
2024-08-11 21:51:00 +0800

    remote clip img size

--- ssr/metrics/remote_clipscore.py
+++ ssr/metrics/remote_clipscore.py
@@ -20,26 +20,27 @@ def calculate_remote_clipscore(img, img2, clip_model, **kwargs):
         if clip_model == 'RN50':
             checkpoint_path = hf_hub_download("chendelong/RemoteCLIP", f"RemoteCLIP-{clip_model}.pt", cache_dir='checkpoints')
             model, _, preprocess = open_clip.create_model_and_transforms(clip_model)
-            img_size = (224,224)
+            #img_size = (224,224)
             logger.info(f'{clip_model} is downloaded to {checkpoint_path}.')
         elif clip_model == 'ViT-B-32':
             checkpoint_path = hf_hub_download("chendelong/RemoteCLIP", f"RemoteCLIP-{clip_model}.pt", cache_dir='checkpoints')
             model, _, preprocess = open_clip.create_model_and_transforms(clip_model)
             model = model.to(device)
-            img_size = (224,224)
+            #img_size = (224,224)
             logger.info(f'{clip_model} is downloaded to {checkpoint_path}.')
         elif clip_model == 'ViT-L-14':
             checkpoint_path = hf_hub_download("chendelong/RemoteCLIP", f"RemoteCLIP-{clip_model}.pt", cache_dir='checkpoints')
             model, _, preprocess = open_clip.create_model_and_transforms(clip_model)
             model = model.to(device)
-            img_size = (224,224)
+            #img_size = (224,224)
             logger.info(f'{clip_model} is downloaded to {checkpoint_path}.')
         else:
             print(clip_model, " is not supported for CLIPScore.")
 
         model = model.cuda().eval()
         remoteClipModel = model
-
+    
+    img_size = (224,224)
     tensor1 = torch.as_tensor(img).permute(2, 0, 1)
     tensor1 = tensor1.unsqueeze(0).to(device).float()/255
     tensor2 = torch.as_tensor(img2).permute(2, 0, 1)
