5b578ae403fc1dd3e102d72981702879dc9e0b17
niklasbloecker <n.bloeckerq@t-online.de>
2024-08-11 21:56:09 +0800

    globalize CLIP model

--- ssr/metrics/clipscore.py
+++ ssr/metrics/clipscore.py
@@ -10,23 +10,37 @@ from basicsr.utils.registry import METRIC_REGISTRY
 
 @METRIC_REGISTRY.register()
 def calculate_clipscore(img, img2, clip_model, **kwargs):
+    global clipModel
     device = torch.device('cuda')
     logger = get_root_logger(logger_name='basicsr', log_level=logging.INFO)
 
+    try:
+        clipModel
+    except NameError:
+        if clip_model == 'clip-ViT-B/16':
+            model, _ = clip.load("ViT-B/16", device=device)
+            model = model.to(device)
+            logger.info(f"{'clip-ViT-B/16'} loaded with img size {img_size}")
+        elif clip_model == 'clipa-ViT-bigG-14':
+            model, _, _ = open_clip.create_model_and_transforms('ViT-bigG-14-CLIPA-336', pretrained='datacomp1b')
+            model = model.to(device)
+            logger.info(f"{'clipa-ViT-bigG-14'} loaded with img size {img_size}")
+        elif clip_model == 'siglip-ViT-SO400M-14':
+            model, _, _ = open_clip.create_model_and_transforms('ViT-SO400M-14-SigLIP-384', pretrained='webli')
+            model = model.to(device)
+            logger.info(f"{'siglip-ViT-SO400M-14'} loaded with img size {img_size}")
+        else:
+            print(clip_model, " is not supported for CLIPScore.")
+        
+        model = model.cuda().eval()
+        clipModel = model
+
     if clip_model == 'clip-ViT-B/16':
-        model, _ = clip.load("ViT-B/16", device=device)
         img_size = (224,224)
-        logger.info(f"{'clip-ViT-B/16'} loaded with img size {img_size}")
     elif clip_model == 'clipa-ViT-bigG-14':
-        model, _, _ = open_clip.create_model_and_transforms('ViT-bigG-14-CLIPA-336', pretrained='datacomp1b')
-        model = model.to(device)
         img_size = (336,336)
-        logger.info(f"{'clipa-ViT-bigG-14'} loaded with img size {img_size}")
     elif clip_model == 'siglip-ViT-SO400M-14':
-        model, _, _ = open_clip.create_model_and_transforms('ViT-SO400M-14-SigLIP-384', pretrained='webli')
-        model = model.to(device)
         img_size = (384,384)
-        logger.info(f"{'siglip-ViT-SO400M-14'} loaded with img size {img_size}")
     else:
         print(clip_model, " is not supported for CLIPScore.")
 
@@ -38,8 +52,8 @@ def calculate_clipscore(img, img2, clip_model, **kwargs):
     tensor1 = F.interpolate(tensor1, img_size)
     tensor2 = F.interpolate(tensor2, img_size)
 
-    feats1 = model.encode_image(tensor1)
-    feats2 = model.encode_image(tensor2)
+    feats1 = clipModel.encode_image(tensor1)
+    feats2 = clipModel.encode_image(tensor2)
 
     clip_score = F.cosine_similarity(feats1, feats2).detach().item()
     logger.info(f"CLIP Score: {clip_score}")
