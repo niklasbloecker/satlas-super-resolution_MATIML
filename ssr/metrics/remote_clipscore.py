684d75184636693cc86f179ad30bf3de59f0e407
niklasbloecker <n.bloeckerq@t-online.de>
2024-08-11 21:43:39 +0800

    globalize remote score

--- ssr/metrics/remote_clipscore.py
+++ ssr/metrics/remote_clipscore.py
@@ -11,30 +11,33 @@ from basicsr.utils.registry import METRIC_REGISTRY
 
 @METRIC_REGISTRY.register()
 def calculate_remote_clipscore(img, img2, clip_model, **kwargs):
+    global remoteClipModel
     device = torch.device('cuda')
     logger = get_root_logger(logger_name='basicsr', log_level=logging.INFO)
 
-    if clip_model == 'RN50':
-        checkpoint_path = hf_hub_download("chendelong/RemoteCLIP", f"RemoteCLIP-{clip_model}.pt", cache_dir='checkpoints')
-        model, _, preprocess = open_clip.create_model_and_transforms(clip_model)
-        img_size = (224,224)
-        logger.info(f'{clip_model} is downloaded to {checkpoint_path}.')
-    elif clip_model == 'ViT-B-32':
-        checkpoint_path = hf_hub_download("chendelong/RemoteCLIP", f"RemoteCLIP-{clip_model}.pt", cache_dir='checkpoints')
-        model, _, preprocess = open_clip.create_model_and_transforms(clip_model)
-        model = model.to(device)
-        img_size = (224,224)
-        logger.info(f'{clip_model} is downloaded to {checkpoint_path}.')
-    elif clip_model == 'ViT-L-14':
-        checkpoint_path = hf_hub_download("chendelong/RemoteCLIP", f"RemoteCLIP-{clip_model}.pt", cache_dir='checkpoints')
-        model, _, preprocess = open_clip.create_model_and_transforms(clip_model)
-        model = model.to(device)
-        img_size = (224,224)
-        logger.info(f'{clip_model} is downloaded to {checkpoint_path}.')
-    else:
-        print(clip_model, " is not supported for CLIPScore.")
+    if remoteClipModel == None:
+        if clip_model == 'RN50':
+            checkpoint_path = hf_hub_download("chendelong/RemoteCLIP", f"RemoteCLIP-{clip_model}.pt", cache_dir='checkpoints')
+            model, _, preprocess = open_clip.create_model_and_transforms(clip_model)
+            img_size = (224,224)
+            logger.info(f'{clip_model} is downloaded to {checkpoint_path}.')
+        elif clip_model == 'ViT-B-32':
+            checkpoint_path = hf_hub_download("chendelong/RemoteCLIP", f"RemoteCLIP-{clip_model}.pt", cache_dir='checkpoints')
+            model, _, preprocess = open_clip.create_model_and_transforms(clip_model)
+            model = model.to(device)
+            img_size = (224,224)
+            logger.info(f'{clip_model} is downloaded to {checkpoint_path}.')
+        elif clip_model == 'ViT-L-14':
+            checkpoint_path = hf_hub_download("chendelong/RemoteCLIP", f"RemoteCLIP-{clip_model}.pt", cache_dir='checkpoints')
+            model, _, preprocess = open_clip.create_model_and_transforms(clip_model)
+            model = model.to(device)
+            img_size = (224,224)
+            logger.info(f'{clip_model} is downloaded to {checkpoint_path}.')
+        else:
+            print(clip_model, " is not supported for CLIPScore.")
 
-    model = model.cuda().eval()
+        model = model.cuda().eval()
+        remoteClipModel = model
 
     tensor1 = torch.as_tensor(img).permute(2, 0, 1)
     tensor1 = tensor1.unsqueeze(0).to(device).float()/255
@@ -44,8 +47,8 @@ def calculate_remote_clipscore(img, img2, clip_model, **kwargs):
     tensor1 = F.interpolate(tensor1, img_size)
     tensor2 = F.interpolate(tensor2, img_size)
 
-    feats1 = model.encode_image(tensor1)
-    feats2 = model.encode_image(tensor2)
+    feats1 = remoteClipModel.encode_image(tensor1)
+    feats2 = remoteClipModel.encode_image(tensor2)
 
     clip_score = F.cosine_similarity(feats1, feats2).detach().item()
     logger.info(f"Remote CLIP Score: {clip_score}")
