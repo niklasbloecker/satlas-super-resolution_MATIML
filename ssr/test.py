67e6ae2ae3c8e88039b1b62d9566bb8bd4e37fe6
niklasbloecker <n.bloeckerq@t-online.de>
2024-08-12 12:51:55 +0800

    test debug

--- ssr/test.py
+++ ssr/test.py
@@ -46,10 +46,7 @@ def test_pipeline(root_path):
 
 
 if __name__ == '__main__':
-    logger = get_root_logger(logger_name='basicsr', log_level=logging.INFO)
-
     start_time = time.time()
     root_path = os.path.abspath(os.path.join(__file__, os.path.pardir, os.path.pardir))
     test_pipeline(root_path)
-
-    logger.info(f'Runtime ---{time.time() - start_time}---')
+    print(f'Runtime ---{time.time() - start_time}---')
